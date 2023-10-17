f9d14284865a3f1b39fb1170d7f300c8cc57ddc8
yutahasegawa178 <hasegawa_yuta@abema.tv>
2023-10-17 18:12:50 +0900

    chore: add thirty multiple adjustments

---
+++ thirty/thirty.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	// 60s以下は一律30sとする
+
+	item := make([]int64, 0, 10)
+	for i := 30; i <= 120; i += 15 {
+		item = append(item, int64(i))
+	}
+	for _, v := range item {
+		println(thirty(v))
+	}
+}
+
+func thirty(value int64) int64 {
+	if value <= 60 {
+		return 30
+	}
+	subtractedValue := value - 30
+	closestMultiple := (subtractedValue / 30) * 30
+	return closestMultiple
+}
