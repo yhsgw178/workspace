ca01d9c7c2f9ba6eab666ac78a7f7b1a2a2f9699
yutahasegawa178 <hasegawa_yuta@abema.tv>
2023-10-24 14:26:18 +0900

    test

--- main.go
+++ main.go
@@ -2,35 +2,8 @@ package main
 
 import "fmt"
 
-type Foo struct {
-	ID    string
-	index int32
-}
-
-type Parent struct {
-	foo []Foo
-}
-
 func main() {
-	parent := Parent{
-		foo: []Foo{
-			{
-				ID:    "a",
-				index: 1,
-			},
-			{
-				ID: "b",
-			},
-		},
-	}
-	ids := []string{"a", "b"}
-	fooMap := make(map[string][]int32)
-	for i := range ids {
-		for j := range parent.foo {
-			if ids[i] == parent.foo[j].ID {
-				fooMap[ids[i]] = append(fooMap[ids[i]], parent.foo[j].index)
-			}
-		}
-	}
-	fmt.Print(fooMap)
+	ids := []string{"a", "b", "c", "d", "e", "a"}
+	data := ids[:len(ids)-1]
+	fmt.Println(data)
 }
