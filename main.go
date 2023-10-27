dff84d99b5522703653d102621163a78b7a9b7e0
yutahasegawa178 <hasegawa_yuta@abema.tv>
2023-10-27 13:59:27 +0900

    chore: fix

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	ids := []string{"a", "b", "c", "d", "e", "a"}
+	ids := []string{"a", "b", "c", "d", "e"}
 	data := ids[:len(ids)-1]
 	fmt.Println(data)
 }
