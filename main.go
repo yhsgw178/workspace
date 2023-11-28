6c42ff89755aa6ff19dc050024dbf97f224e2acc
yutahasegawa178 <hasegawa_yuta@abema.tv>
2023-11-28 17:45:20 +0900

    chore: time fmt

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
+// You can edit this code!
+// Click here and start typing.
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+const dateTimeFormatWithSecond = "2006/01/02 15:04:05"
+
 func main() {
+	JST := time.FixedZone("JST", 9*3600)
+	ut := time.Now().Unix()
+	str := time.Unix(ut, 0).In(JST).Format(dateTimeFormatWithSecond)
+	fmt.Println(str)
 }
