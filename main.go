4b1e506f2d8bbfe09c78106d1122fc72ad20ffc9
yutahasegawa178 <hasegawa_yuta@abema.tv>
2023-12-04 18:28:34 +0900

    commit

--- main.go
+++ main.go
@@ -2,16 +2,5 @@
 // Click here and start typing.
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-const dateTimeFormatWithSecond = "2006/01/02 15:04:05"
-
 func main() {
-	JST := time.FixedZone("JST", 9*3600)
-	ut := time.Now().Unix()
-	str := time.Unix(ut, 0).In(JST).Format(dateTimeFormatWithSecond)
-	fmt.Println(str)
 }
