041edafb8d5eff316b13fa78221cb37ceb3c7a49
yutahasegawa178 <hasegawa_yuta@abema.tv>
2023-12-08 15:56:49 +0900

    chore: try sort

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
+	m := map[string]string{"a3": "a", "a5": "e", "a2": "b", "a1": "a", "a4": "d"}
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	fmt.Println(keys)
 }
